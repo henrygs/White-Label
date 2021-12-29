ab66d5d4895a2ee2c21d3f1901be7f645349d8f7
henrygs <henrique-91@hotmail.com>
2021-12-29 09:41:15 -0300

    Create add of product

---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/addproduct/AddProductFragment.kt
@@ -0,0 +1,89 @@
+package br.com.douglasmotta.whitelabeltutorial.ui.products.addproduct
+
+import android.net.Uri
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import br.com.douglasmotta.whitelabeltutorial.databinding.AddProductFragmentBinding
+import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputLayout
+
+class AddProductFragment : BottomSheetDialogFragment() {
+
+    private var _binding: AddProductFragmentBinding? = null
+    private val binding get() = _binding!!
+    private var imageUri: Uri? = null
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            imageUri = uri
+            binding.imageProduct.setImageURI(uri)
+        }
+
+    private lateinit var viewModel: AddProductViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = AddProductFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeVMEventes()
+        setListeners()
+    }
+
+    private fun observeVMEventes() {
+        viewModel.imageUriErrorResId.observe(viewLifecycleOwner) { drawableResId ->
+            binding.imageProduct.setBackgroundResource(drawableResId)
+        }
+
+        viewModel.descriptionFieldErrorResId.observe(viewLifecycleOwner) { stringResId ->
+            binding.inputLayoutDescription.setError(stringResId)
+        }
+
+        viewModel.priceFieldErrorResId.observe(viewLifecycleOwner) { stringResId ->
+            binding.inputLayoutPrice.setError(stringResId)
+        }
+
+    }
+
+    private fun TextInputLayout.setError(stringResId: Int?) {
+        error = if (stringResId != null) {
+            getString(stringResId)
+        } else null
+
+    }
+
+
+    private fun setListeners() {
+
+        binding.imageProduct.setOnClickListener {
+            choseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+
+            viewModel.createProduct(description, price, imageUri)
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun choseImage() {
+        getContent.launch("image/*")
+    }
+}
